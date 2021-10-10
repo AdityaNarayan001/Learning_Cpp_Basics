4886ddc24df4c3642e9c7dcd4c23fc83dd6c9932
Aditya Narayan <86866968+AdityaNarayan001@users.noreply.github.com>
2021-10-10 19:02:32 +0530

    Delete C++ Basics directory

--- C++ Basics/OOP.cpp
+++
@@ -1,4 +0,0 @@
-// using class is different from procedural coding that stores the prcocedure and function to perform some function on data
-// classes conatin objects functions and data
-
-// objects are instance of class and class is template for object
--- C++ Basics/boolean.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-    cout << (9 < 10);
-    //output is 1 which means True and 0 means False
-    return 0;
-}
